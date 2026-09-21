4267f519f83ce25b4eb0669384d8ebac736a0b65
roman-petruzela <roman@petruze.la>
2026-09-21 14:45:17 +0200

    Add SelectSort

---
+++ SelectSort/SelectSort/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelectSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pr1();
+        }
+
+        public static void selectionSortVz(int[] pole)
+        {
+            for (int j = 0; j < pole.Length - 1; j++)
+            {
+                int pozice = j;
+                for (int i = j + 1; i < pole.Length; i++)
+                    if (pole[i] < pole[pozice])
+                        pozice = i;
+                int tmp = pole[j];
+                pole[j] = pole[pozice];
+                pole[pozice] = tmp;
+            }
+        }
+        public static void selectionSortS(int[] pole)
+        {
+            for (int j = 0; j < pole.Length - 1; j++)
+            {
+                int pozice = j;
+                for (int i = j + 1; i < pole.Length; i++)
+                    if (pole[i] > pole[pozice])
+                        pozice = i;
+                int tmp = pole[j];
+                pole[j] = pole[pozice];
+                pole[pozice] = tmp;
+            }
+        }
+        public static void pr1()
+        {
+            int[] pole = {10,1,2,15,-1,8};
+            selectionSortS(pole);
+            foreach (int x in pole) Console.Write(x + "; ");
+            Console.WriteLine();
+            selectionSortVz(pole);
+            foreach (int x in pole) Console.Write(x + "; ");
+            Console.ReadKey();
+        }
+
+
+    }
+}
